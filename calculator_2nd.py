8f996edb9e80ee4d2d4bdca8f19d3bfaf7dc64d3
Nahid Ferdous <nahid.mferdous@gmail.com>
2022-03-19 17:41:10 -0500

    Create calculator_2nd.py
    In this section we used;
    While loop, Flag and Recursion

---
+++ calculator_2nd.py
@@ -0,0 +1,39 @@
+
+def add(number1,number2):
+    return number1+number2
+def subtract(number1,number2):
+        return number1-number2
+def multiply(number1,number2):
+    return number1*number2
+def devide(number1,number2):
+    return number1/number2
+def oparetions1():
+    for key in oparetions:
+        print(key)
+oparetions={
+    "+":add,
+    "-":subtract,
+    "*":multiply,
+    "/":devide
+}
+
+num1=int(input("Enter your first number: "))
+oparetions1()
+action="Yes"
+while action!="No":
+    symtable=input("Select your symble: ")
+    num2=int(input("enter the second number: "))
+    answer1= oparetions[symtable](num1,num2)
+
+    oparetions1()
+    symtable2=input("Select your Symbol: ")
+    num3=int(input("Enter your third number: "))
+    answer2=oparetions[symtable2](answer1,num3)
+    print(answer2)
+    if input("Select Yes or No ")=="Yes":
+       num1= answer2
+    else:
+        action="No"
+
+    
+
